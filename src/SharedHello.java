a06141c1174c972b5693fd1397539fe55f817850
mrjbhl <70877369+mrjbhl@users.noreply.github.com>
2020-09-11 10:47:40 +0200

    Commit 1 Jens

---
+++ src/SharedHello.java
@@ -0,0 +1,29 @@
+public class SharedHello {
+
+    public static void main(String[] args) {
+
+        // Display message Welcome to Java! on the console
+        System.out.println("Welcome to Java!");
+        // Skrevet af Jens
+        hej("Jens");
+        godDag("Jens");
+
+        // Skrevet af Lukas
+
+
+
+        // Skrevet af Asta
+
+    }
+
+    // Her er et eksempel på en metode
+    public static void hej(String navn) {
+        System.out.print("Hej " + navn  + "! ");
+    }
+
+    // Her er et eksempel på en anden metode
+    public static void godDag(String navn) {
+        System.out.println("Velkommen og hav en god dag " + navn  + "!");
+    }
+
+}
